0573f385fb0b85580fe7e5b5febe832c20e06006
Yodugo <hlosada04@gmail.com>
2025-06-12 03:08:13 +0200

    platform and station starting development

--- src/pw1/Platform.cs
+++ src/pw1/Platform.cs
@@ -2,8 +2,45 @@ using System;
 
 namespace PwI_Extra
 {
+    public enum PlatformStatus
+    {
+        Free,
+        Occupied
+    }
     public class Platform
     {
-        
+        public string id;
+        public PlatformStatus status;
+        public Train currentTrain;
+        public int dockingTime;
+
+        public Platform(string id, PlatformStatus status, Train currentTrain, int dockingTime)
+        {
+            this.id = id;
+            this.status = status;
+            this.currentTrain = currentTrain;
+            this.dockingTime = dockingTime;
+
+        }
+
+        public string GetId()
+        {
+            return this.id;
+        }
+        public PlatformStatus GetStatus()
+        {
+            return this.status;
+        }
+        public Train GetCurrentTrain()
+        {
+            return this.currentTrain;
+        }
+        public int GetDockingTime()
+        {
+            return this.dockingTime;
+        }
+
+
+
     }
 }
--- src/pw1/Station.cs
+++ src/pw1/Station.cs
@@ -4,6 +4,21 @@ namespace PwI_Extra
 {
     public class Station
     {
+        public List<Platform> Platforms = new List<Platform>();
+        public List<Train> Trains = new List<Train>();
+
+        public Station()
+        {
+
+        }
+        public void DisplayStatus()
+        {
+
+        }
+        public void AdvanceTick()
+        {
+            
+        }
         
     }
 }
